2dc224226a35f79495640ee83aacb18607418652
Mogahed Ahmed <mujahedahmed81@gmail.com>
2025-03-28 15:33:00 +0300

    تفعيل عرض media files في production

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('vote/', include('vote.urls')),      # التصويت
-    path('news/', include('news.urls')),      # الأخبار الديناميكية
+    path('vote/', include('vote.urls')),
+    path('news/', include('news.urls')),
     path('contact/', include('contact.urls')),
     path('activities/', include('activities.urls')),
     path('regulations/', include('regulations.urls')),
-    path('contact/', include('contact.urls')),
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
